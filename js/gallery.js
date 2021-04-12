459a9a42479e29e7715852ca04c1ad1712e8748a
Ivan Laver <74679323+Ereyzer@users.noreply.github.com>
2021-04-12 22:16:07 +0300

    krasa

--- js/gallery.js
+++ js/gallery.js
@@ -10,9 +10,6 @@ const refs = {
 }
 
 refs.galleryList.addEventListener('click', addIsOpen);
-refs.closeBtn.addEventListener('click', removeIsOpen);
-
-
 
 function makeListElements(array) {
     const itemOfGallery = array.map(({preview, description}) =>
@@ -21,23 +18,23 @@ function makeListElements(array) {
     return itemOfGallery;
 };
 
-refs.galleryList.insertAdjacentHTML('beforeend', `${makeListElements(photos)}`)
+refs.galleryList.innerHTML = `${makeListElements(photos)}`;
 
 function addIsOpen(event) {
     if (event.target.nodeName !== 'IMG') {
         return;
-    }  
-    
-    openBigPhoto(event);
+    };
+    makeBigPhoto(bigSizeOfImage(event.target));
     refs.lightbox.classList.add('is-open');
     window.addEventListener('keydown', checkKeyboardEvent);
     refs.overlay.addEventListener('click', removeIsOpen);
+    refs.closeBtn.addEventListener('click', removeIsOpen);
     refs.galleryList.removeEventListener('click', addIsOpen);
 };
 
 function removeIsOpen(event) {
-console.log(event);
     window.removeEventListener('keydown', checkKeyboardEvent);
+    refs.closeBtn.removeEventListener('click', removeIsOpen);
     refs.overlay.removeEventListener('click', removeIsOpen);
     refs.galleryList.addEventListener('click', addIsOpen);
     refs.lightbox.classList.remove('is-open');
@@ -47,54 +44,45 @@ console.log(event);
 
 function checkKeyboardEvent(event) {
     if (event.code === 'ArrowLeft') {
-        scrollLeft(event)
+        scrollLeft()
     }else if (event.code === 'ArrowRight') {
-        scrollRight(event);
+        scrollRight();
     }else if (event.code === 'Escape'){
         removeIsOpen(event);
     }else{
         return;
     }
 };
-const indexOfBigImg = () => photos.reduce((acc, img, index) => {
 
-        if (img.description === refs.bigImg.alt) {
-            acc = index;
-        }
-        return acc;
-    },0);
+const indexOfBigImg = () => {
+    for(let i = 0; i < photos.length; i += 1){
+        if (photos[i].description === refs.bigImg.alt) {
+            return i;
+        }  
+    }
+};
 
 
-
-function openBigPhoto(event) {
-    const bigPhoto = bigSizeOfImage(event.target);
-    refs.bigImg.src = `${bigPhoto.original}`;
-    refs.bigImg.alt = `${bigPhoto.description}`;
-}
-
-function scrollRight(event) {
+function scrollRight() {
     let index = indexOfBigImg() + 1;
     if(index === photos.length){
-
         index = 0;
     }
 
-    const bigPhoto = photos[index];
-
-    refs.bigImg.src = `${bigPhoto.original}`;
-    refs.bigImg.alt = `${bigPhoto.description}`;
+    makeBigPhoto(photos[index])
 }
 
-function scrollLeft(event) {
+function scrollLeft() {
     let index = indexOfBigImg() - 1;
     if(index === -1){
-        console.log(photos.length);
         index = photos.length - 1;
     }
-    const bigPhoto = photos[index];  
-    refs.bigImg.src = `${bigPhoto.original}`;
-    refs.bigImg.alt = `${bigPhoto.description}`;
+    makeBigPhoto(photos[index])
 }
 
+function makeBigPhoto(bigPhoto) {
+    refs.bigImg.src = `${bigPhoto.original}`;
+    refs.bigImg.alt = `${bigPhoto.description}`;
+};
 const  bigSizeOfImage = (currentImg) => photos.find(img => img.description === currentImg.alt);
 
